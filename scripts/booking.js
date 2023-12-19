25c4c2437f8492a3e50452f77046601ff687abcb
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-12-19 15:46:46 +0000

    finished booking page

---
+++ scripts/booking.js
@@ -0,0 +1,48 @@
+const servicePrices = {
+    "wig_fixing": {
+        "name": "Wig Fixing",
+        "price": "100"
+    },
+    "wig_sewing": {
+        "name": "Wig Sewing",
+        "price": "100"
+    },
+    "braides": {
+        "name": "Braides",
+        "price": "100"
+    },
+    "bridal_styling": {
+        "name": "Bridal Styling",
+        "price": "100"
+    },
+    "hair_coloring": {
+        "name": "Hair Coloring",
+        "price": "100"
+    },
+}
+
+const state = {
+    "regular": "0.00",
+    "express": "100.00"
+}
+
+const service = document.querySelector("#service");
+const stateRadios = document.querySelectorAll('input[name="state"]');
+
+const price = document.querySelector("#price");
+
+stateRadios.forEach(radio => {
+    radio.addEventListener("change", function () {
+        selectedService = service.value
+        const serviceName = servicePrices[selectedService].name;
+        const servicePrice = servicePrices[selectedService].price;
+        if (radio.checked) {
+            statePrice = state[radio.value]
+            price.value = ""; //refresh
+            price.value = `
+            ${serviceName} GHC${servicePrice}
+            Charges GHC${statePrice}
+            `
+        }
+    });
+});
